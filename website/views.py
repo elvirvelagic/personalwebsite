916284bb6f0cbd74e5468976f11a73e7755e1060
Elvir <elvir.1998@hotmail.com>
2023-02-22 17:12:24 +0100

    new update

--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return '<hi>Test</h1>'
+    return render_template('base.html')
     
 
